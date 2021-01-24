d6e276698a773fd07fbe877545d8ccba291db7a0
Kapilan Navaratnam <kapilannava@gmail.com>
2021-01-24 01:37:59 -0500

    Added Lottie animation component

---
+++ src/components/Lottie.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "@lottiefiles/lottie-player";
+
+const Lottie = () => {
+
+    return (
+        <div className="m-0 m-auto">
+<lottie-player
+  autoplay
+  loop
+  mode="normal"
+  src="https://assets8.lottiefiles.com/packages/lf20_zETGpb.json"
+  style={{width: 320, margin: "0 auto" }}
+>
+</lottie-player>
+</div>
+    )
+}
+
+export default Lottie;
