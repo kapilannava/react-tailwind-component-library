46596feaebb98f8757c7a60e2ae9c9aac53adc62
Kapilan Navaratnam <kapilannava@gmail.com>
2021-01-25 10:49:18 -0500

    Test commit

--- src/SectionPage.js
+++ src/SectionPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SectionPage = () => {
     return (
         <div>
-            This is the SectionPage
+            This is the SectionPage!
         </div>
     )
 }
