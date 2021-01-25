1ea41419230847c71e73caf37116e59ce59c025b
Kapilan Navaratnam <kapilannava@gmail.com>
2021-01-24 23:33:01 -0500

    Added footer to Homepage

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 const Home = () => {
     return (
-        <div>
+            <div className="flex flex-col h-screen justify-between">
             <Hero />
-        </div>
+<Footer />
+</div>
     )
 }
 
